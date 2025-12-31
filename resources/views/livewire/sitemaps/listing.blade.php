c9a31becfa8ae62d0d29283840f72e6ee0e2f854
Angelovski <angelovski@angelovski.angelovski.com>
2026-01-01 00:50:24 +0100

    Sitemaps table styling

--- resources/views/livewire/sitemaps/listing.blade.php
+++ resources/views/livewire/sitemaps/listing.blade.php
@@ -8,41 +8,55 @@
 
     <x-page-header :name="$website->name" :url="$website->url" />
 
-    <div class="space-y-4">
-        <table class="w-full rounded-xl border border-gray-200">
-            <thead class="">
-                <tr>
-                    <th class="px-4 py-2 text-left">URL</th>
-                    <th class="px-4 py-2 text-left">Created</th>
-                </tr>
-            </thead>
+    <div class="flex flex-col rounded-xl border border-gray-200 bg-white dark:border-zinc-800 dark:bg-zinc-900">
 
-            <tbody>
-                @forelse ($sitemaps as $sitemap)
-                    <tr class="border-t">
-                        <td class="px-4 py-2 text-sm">
-                            <a href="{{ $sitemap->url }}" target="_blank" rel="noopener noreferrer"
-                                class="hover:underline truncate block">
-                                {{ $sitemap->url }}
-                            </a>
-                        </td>
-                        <td class="px-4 py-2 text-sm">
-                            {{ $sitemap->created_at->format('Y-m-d H:i') }}
-                        </td>
-                    </tr>
-                @empty
-                    <tr>
-                        <td colspan="2" class="px-4 py-6 text-center">
-                            No sitemaps found.
-                        </td>
-                    </tr>
-                @endforelse
-            </tbody>
-        </table>
 
-        {{-- Pagination --}}
-        <div class="">
-            {{ $sitemaps->links() }}
+        <div class="overflow-auto">
+            <div class="p-4">
+                {{-- Table Header --}}
+                <table class="w-full text-sm table-fixed">
+                    <thead class="text-left text-gray-500 dark:text-zinc-400">
+                        <tr class="border-b border-gray-200 dark:border-zinc-800">
+                            <th class="pb-2 pr-4">URL</th>
+                            <th class="pb-2 pr-4">Created</th>
+                        </tr>
+                    </thead>
+                </table>
+
+                {{-- Scrollable tbody --}}
+                <div class="overflow-y-auto" style="max-height: 400px;">
+                    <table class="w-full text-sm table-fixed">
+                        <tbody>
+                            @forelse ($sitemaps as $sitemap)
+                                <tr
+                                    class="border-b  border-gray-200 hover:bg-gray-50 dark:border-zinc-800/50 dark:hover:bg-zinc-800/30 text-gray-700 dark:text-zinc-300">
+                                    <td class="py-3 pr-4">
+                                        <a href="{{ $sitemap->url }}" target="_blank" rel="noopener noreferrer"
+                                            class="hover:underline truncate block">
+                                            {{ $sitemap->url }}
+                                        </a>
+                                    </td>
+                                    <td class="py-3 pr-4 text-gray-400 dark:text-zinc-400">
+                                        {{ $sitemap->created_at->format('Y-m-d H:i') }}</td>
+
+                                </tr>
+                            @empty
+                                <tr>
+                                    <td colspan="4" class="text-center py-8 text-gray-500 dark:text-zinc-500">
+                                        No sitemaps found
+                                    </td>
+                                </tr>
+                            @endforelse
+                        </tbody>
+                    </table>
+                </div>
+
+                {{-- Pagination --}}
+
+                <div class="mt-4">
+                    {{ $sitemaps->links() }}
+                </div>
+            </div>
         </div>
     </div>
 </div>
